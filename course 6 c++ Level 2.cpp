89b6131eb4f372f4cda83af4f9f730212bf95d88
Ahmed alamin <narout2015@gmail.com>
2026-01-22 11:57:57 +0300

    Lesson # 27 - Two Dimensional Arrays

--- course 6 c++ Level 2.cpp
+++ course 6 c++ Level 2.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <iomanip>
 #include "MyRead.h"
+
 using namespace std;
-
-
+//int fillArray ()
 int main()
 {
+	int Arr[10][10];
 	
-	int Number = MyRead::ReadNumber();
-	printf("The Number You Entered Is : %d", Number);
-	return 0;
+	for (int i = 0; i < 10; i++)
+	{ 
+		for (int j = 0; j <10; j++)
+		{
+			Arr[i][j] = (i + 1) * (j + 1);
+		}
+		
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			printf("%0*d ",2 , Arr[i][j]);
+		}
+		cout << endl;
+	}
 }
 	
 
