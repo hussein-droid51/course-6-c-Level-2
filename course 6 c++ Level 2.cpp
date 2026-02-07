e6be12f3821abcf7eb60e89f249dc36eb50c1811
Ahmed alamin <narout2015@gmail.com>
2026-02-07 11:51:52 +0300

    Learn how to deal with files

--- course 6 c++ Level 2.cpp
+++ course 6 c++ Level 2.cpp
@@ -1,30 +1,94 @@
 #include <iostream>
+#include<vector>
 #include <iomanip>
 #include "MyRead.h"
+#include<string>
+#include<cctype>
+#include<fstream>
 
 using namespace std;
-//int fillArray ()
+
+void LodDataFromFileToVector(string FileName,vector<string>& VFileContent)
+{
+	fstream MyFile;
+	MyFile.open("MyFile.txt", ios::in);//Read Mod 
+
+		if (MyFile.is_open())
+		{
+			string Line;
+			while(getline(MyFile,Line))
+
+			{
+				VFileContent.push_back(Line);
+			}
+				
+		}
+		MyFile.close();
+}
+
+void SaveVectorToFile(string FileName, vector<string> VFileContent)
+{
+	fstream MyFile;
+	MyFile.open("MyFile.txt", ios::out);//Write Mod 
+
+	if (MyFile.is_open())
+	{
+		for (string& Line : VFileContent)
+		{
+			if (Line != "")
+			{
+				MyFile << Line<<endl;
+			}
+		}
+	}
+	MyFile.close();
+}
+
+
+void DeleteRecordFromFile(string FileName, string Record, string UpdateTo)
+{
+	vector <string>VFileContent;
+	LodDataFromFileToVector(FileName, VFileContent);
+
+	
+		for (string &Line : VFileContent)
+		{
+			if (Line==Record)
+			{
+				Line=UpdateTo;
+			}
+		}
+		SaveVectorToFile(FileName,VFileContent);
+}
+
+void PrintFileContent(string FileName)
+{
+	fstream MyFile;
+	MyFile.open(FileName,ios::in );//Read Mode 
+
+	if (MyFile.is_open())
+	{
+		string Line;
+
+		while (getline(MyFile,Line))
+		{
+			cout << Line<<endl;
+		}
+		MyFile.close();
+	}
+}
+
 int main()
 {
-	int Arr[10][10];
+	cout << "File content Befor Deleting it  " << endl;
+	 PrintFileContent("MyFile.txt");
 	
-	for (int i = 0; i < 10; i++)
-	{ 
-		for (int j = 0; j <10; j++)
-		{
-			Arr[i][j] = (i + 1) * (j + 1);
-		}
-		
-	}
+	DeleteRecordFromFile("MyFile.txt", "Ail","osman");
 
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			printf("%0*d ",2 , Arr[i][j]);
-		}
-		cout << endl;
-	}
+	cout << "content After Deleting it " << endl;
+	PrintFileContent("MyFile.txt");
+
+	
 }
 	
 
